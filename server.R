c4b1d895a6c4dc50899d8f54ae7a8f25e53f695f
fredschade <fredschade@gmx.de>
2018-11-06 10:52:21 +0100

    fixed new order

--- server.R
+++ server.R
@@ -82,9 +82,7 @@ shinyServer(function(input, output,session) {
   source("server_Method.R",local = T)
 
   # connect with the hardware and use printcore to control
-  connect = reactiveValues(board = board)
-
-  source_python("oc_driver/printrun/printcore.py")
+  connect = reactiveValues(board = board)y
 
   testConnection()
 
